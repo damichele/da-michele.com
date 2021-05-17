056fdc2a8a4a3ee21e6020599d8c6b65ac7f7821
Johannes Rappen <jrappen@outlook.com>
2021-05-17 03:08:19 +0200

    chore: remove email address from footer

--- packages/website/.vuepress/components/RestaurantAddress.vue
+++ packages/website/.vuepress/components/RestaurantAddress.vue
@@ -7,6 +7,25 @@
     </address>
     <p>
       <a
+        :href="$withBase($localePath + 'vcards/Da_Michele_Baden-Baden.vcf')"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <svg
+          role="img"
+          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M2,3H22C23.05,3 24,3.95 24,5V19C24,20.05 23.05,21 22,21H2C0.95,21 0,20.05 0,19V5C0,3.95 0.95,3 2,3M14,6V7H22V6H14M14,8V9H21.5L22,9V8H14M14,10V11H21V10H14M8,13.91C6,13.91 2,15 2,17V18H14V17C14,15 10,13.91 8,13.91M8,6A3,3 0 0,0 5,9A3,3 0 0,0 8,12A3,3 0 0,0 11,9A3,3 0 0,0 8,6Z" />
+        </svg>
+        <span v-if="$lang == 'en-US'">Download vCard</span>
+        <span v-else>Visitenkarte herunterladen</span>
+      </a>
+    </p>
+    <p>
+      <a
         href="tel:+49722161541"
         target="_blank"
         rel="noopener noreferrer"
@@ -39,25 +58,6 @@
           <path d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" />
         </svg>
         <span>info@da-michele.com</span>
-      </a>
-    </p>
-    <p>
-      <a
-        :href="$withBase($localePath + 'vcards/Da_Michele_Baden-Baden.vcf')"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <svg
-          role="img"
-          viewBox="0 0 24 24"
-          width="16"
-          height="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M2,3H22C23.05,3 24,3.95 24,5V19C24,20.05 23.05,21 22,21H2C0.95,21 0,20.05 0,19V5C0,3.95 0.95,3 2,3M14,6V7H22V6H14M14,8V9H21.5L22,9V8H14M14,10V11H21V10H14M8,13.91C6,13.91 2,15 2,17V18H14V17C14,15 10,13.91 8,13.91M8,6A3,3 0 0,0 5,9A3,3 0 0,0 8,12A3,3 0 0,0 11,9A3,3 0 0,0 8,6Z" />
-        </svg>
-        <span v-if="$lang == 'en-US'">Download vCard</span>
-        <span v-else>Visitenkarte herunterladen</span>
       </a>
     </p>
   </div>
